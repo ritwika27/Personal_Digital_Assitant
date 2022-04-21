aa40f963ec5fa995ff8c21a7690d72a37c6f44e5
farnazzam <far.zamiri75@gmail.com>
2022-04-21 18:14:23 -0400

    flask server

---
+++ dashboard/server.py
@@ -0,0 +1,40 @@
+import csv
+try:
+    import simplejson as json
+except ImportError:
+    import json
+from flask import Flask,request,Response,render_template
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/')
+def renderPage():
+  return render_template("index.html")
+
+@app.route('/preferences')
+def preferences():
+  con = psycopg2.connect(
+            database = "postgres",
+            user = "farnazzamiri",
+            password = "pgadmin")
+  cur = con.cursor()
+
+  cur.execute(f"""
+        SELECT *
+        FROM
+            public."userData";
+        """)
+  pref = cur.fetchall()
+
+  cur.close()
+  con.close()
+
+  print(pref)
+  data = json.dumps(pref)
+  resp = Response(data, status=200, mimetype='application/json')
+  print(resp)
+  return resp
+
+if __name__ == "__main__":
+  app.run(debug=True,port=8000)
