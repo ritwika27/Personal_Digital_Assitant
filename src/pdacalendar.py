84ed733bf5c02723de8c6d01fcc62142bfc61f9d
leo <liuleo1998@gmail.com>
2022-04-25 17:51:07 -0400

    demo

--- src/pdacalendar.py
+++ src/pdacalendar.py
@@ -58,11 +58,32 @@ class Calendar:
             msg.msg["msg"] = "pong"
             a.send(msg.reply(msg.msg, msg.msg_type))
 
-
-
-
     def add_event(self, start_time, end_time):
         pass
+
+    def sync(self):
+        try:
+            service = build('calendar', 'v3', credentials=self.creds)
+
+            # Call the Calendar API
+            now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+            print('Getting the upcoming 10 events')
+            events_result = service.events().list(calendarId='primary', timeMin=now,
+                    maxResults=10, singleEvents=True,
+                    orderBy='startTime').execute()
+            events = events_result.get('items', [])
+
+            if not events:
+                print('No upcoming events found.')
+                return
+
+            # Prints the start and name of the next 10 events
+            for event in events:
+                start = event['start'].get('dateTime', event['start'].get('date'))
+                print(start, event['summary'])
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+
 
     def test(self):
         """Shows basic usage of the Google Calendar API.
