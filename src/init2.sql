a5e4ac924a5c5e236fe1df36b56fc44202435ff5
farnazzam <far.zamiri75@gmail.com>
2022-05-04 18:18:10 -0400

    sql init2

---
+++ src/init2.sql
@@ -0,0 +1,25 @@
+-- Table: public.userData
+
+DROP TABLE IF EXISTS public."userData";
+
+CREATE TABLE IF NOT EXISTS public."userData"
+(
+    event_id numeric,
+    preferences character varying COLLATE pg_catalog."default",
+    user_location character varying COLLATE pg_catalog."default",
+    user_lat numeric,
+    user_long numeric,
+    event_location character varying COLLATE pg_catalog."default",
+    event_lat numeric,
+    event_long numeric,
+    event_start_time timestamp without time zone,
+    event_end_time timestamp without time zone,
+    event_title character varying COLLATE pg_catalog."default",
+    event_description character varying COLLATE pg_catalog."default",
+    event_passed numeric
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."userData"
+    OWNER to postgres;
