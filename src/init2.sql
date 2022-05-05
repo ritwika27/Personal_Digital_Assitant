fd7bc8915377a72898823ad324896a0e861245c9
Jason Tan <10.1.19.15.14.tan@gmail.com>
2022-05-04 23:13:23 -0400

    Pull event data from database

--- src/init2.sql
+++ src/init2.sql
@@ -23,3 +23,7 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."userData"
     OWNER to postgres;
+
+INSERT INTO public."userData" (event_id, preferences, user_location, user_lat, user_long, event_location, event_lat, event_long, event_start_time, event_end_time, event_title, event_description, event_passed) VALUES
+(1, 'car', 'Democracy blvd', 39.022797, -77.151316, 'College park', 38.991385, -76.937700, '2022-05-11 11:00:00', '2022-05-11 12:00:00', 'Daily Standup', 'work meeting', 0),
+(2, 'bus', 'Democracy blvd', 39.022797, -77.151316, 'Paint branch Dr', 38.997691, -76.940289, '2022-05-11 13:00:00', '2022-05-11 13:30:00', 'Weekly Standup', 'work meeting', 0);
